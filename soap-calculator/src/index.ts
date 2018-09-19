d9c029fe8da83f40757af8efbc03a08691123a2f
f-w <fred.wen@gov.bc.ca>
2018-09-19 09:34:37 -0700

    added soap-calculator

---
+++ soap-calculator/src/index.ts
@@ -0,0 +1,16 @@
+import {SoapCalculatorApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {SoapCalculatorApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new SoapCalculatorApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
